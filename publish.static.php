6468a58caf46dd3b12e98f90cd7f1ccba5e0a34c
vinceMeens <v.w.a.meens@comcordis.com>
2013-09-11 01:22:58 +0200

    Static publishing per project

---
+++ publish.static.php
@@ -0,0 +1,12 @@
+<?php
+
+XXX_Static_Publisher::publishMergedFilesFromOtherProject($project, $deployIdentifier, array
+(
+	'XXX_Component_Calendar.js',
+	'XXX_Component_MultipleDatesPickerCalendar.js',
+	'XXX_Component_Clock.js',
+	'XXX_Component_OpeningWeekdaysAndHours.js'
+),
+'initialize.js');
+
+?>
